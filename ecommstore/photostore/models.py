28f247389f740aa1f989a7e3568bbd05e881f904
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-02 22:04:46 +0200

    build initial models

--- ecommstore/photostore/models.py
+++ ecommstore/photostore/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+from users.models import UserProfile
+
 # Create your models here.
+
+class Customer(models.Model): 
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    fullname = models.CharField('Full Name', max_length=128)
+    type = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='customers')
+
+    def save_fullname(self, *args, **kwargs):
+        self.username = f"{self.user.first_name} {self.user.last_name}"
+        super().save_fullname(*args, **kwargs)
+
+
+
+class Product(models.Model):
+    TYPE_CHOICES = [
+        ('PH', 'Photo'),
+        ('ART', 'Art'),
+    ]
+
+    THEME_CHOICES = [
+        ('mountain', 'Mountain'),
+        ('lake', 'Lake'),
+        ('polar', 'Polar'),
+        ('forest', 'Forest'),
+        ('tree','Tree'),
+        ('flowers', 'Flowers'),
+    ]
+
+    SIZE_CHOICES =[
+        ('L', 'Big'),
+        ('M', 'Medium'),
+        ('S', 'Small'),
+    ]
+
+    STATUS_CHOICES = [
+        ('AVL', 'Available'),
+        ('OOPS', 'Out of Stock')
+    ]
+    author = models.CharField('Author (full name)', max_length=72)  # associate to UserProfile Model
+    type = models.CharField('Type', max_length=10, choices=TYPE_CHOICES)
+    theme = models.CharField('Theme', max_length=20, choices=THEME_CHOICES)
+    size = models.CharField('Resolution', max_length=10, choices=SIZE_CHOICES)
+    item_url = models.URLField('URL', max_length=250, unique=True)
+    status = models.CharField('Status', max_length=30, choices=STATUS_CHOICES)
--- ecommstore/users/models.py
+++ ecommstore/users/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserProfile(AbstractUser):
+    USER_CHOICES = [
+        ('ART', 'Artist'),
+        ('PHT', 'Photographer'),
+        ('ENT', 'Enthusiast')
+    ]
+
+    # AbstractUser model fields - first_name, last_name, email, username, password, is_staff, is_active, date_joined
+    # define additional or custom fields
+ 
+    login_name = models.CharField('Username (auto-generated)', max_length=150, unique=True, null=True)
+ 
+    # (py311-webdev) D:\GitHub_Projects\DevSchool-Project\ecommstore>python manage.py makemigrations
+    # It is impossible to add a non-nullable field 'login_name' to userprofile without specifying a default. This is because the database needs something to populate existing rows.
+    # Please select a fix:
+    # 1) Provide a one-off default now (will be set on all existing rows with a null value for this column)
+    # 2) Quit and manually define a default value in models.py.
+
+    customer_type = models.CharField('Customer Type', max_length=20, choices=USER_CHOICES)
+    bio = models.TextField('Bio', max_length=200, null=True, blank=True)
+
+    # define related_name to resolve clashes with User module
+    groups = models.ManyToManyField('auth.Group', related_name='userprofile')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='userprofile')
+
+    def save_username(self, *args, **kwargs):
+        """
+        Saves the current object to the database.
+
+        This method generates a unique username for the object by combining the lowercase
+        first name and last name with a random 4-digit suffix. The generated username is
+        then assigned to the `username` attribute of the object.
+        """
+        import random
+
+        suffix = str(random.randint(1000, 9999))
+        self.username = f"{self.fname.lower()}{self.lname.lower()}_{suffix[:4]}"
+        super().save_username(*args, **kwargs)
+
