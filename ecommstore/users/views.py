41ac80d477f6346575c40b3dc47ae92827d1cb45
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-07 21:14:18 +0200

    update login, signup

--- ecommstore/users/views.py
+++ ecommstore/users/views.py
@@ -2,36 +2,62 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
-def login(request):
+from users.forms import LoginForm, RegForm
+
+# Views
+# REGISTRATION
+def signup(request):
+    # import UserProfile, password hashing lib
+    from users.models import UserProfile
+    from django.contrib.auth.hashers import make_password
+    
+    # validate request
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        form = RegForm(request.POST)
 
-        user = authenticate(request, username, password)
-        
-        if user:
-            login(request, user)
-            request.session["username"] = user.username
-            return HttpResponseRedirect(reverse('photostore:index'))
-        
+        if form.is_valid():
+            # capitalize name
+            
+            # hash user-provided password
+            raw_password = form.cleaned_data["password"]
+            hashed_password = make_password(raw_password)
+            user = UserProfile(password=hashed_password)
+            user.save()
+            # form.cleaned_data["password"]
+            # form.save() 
+
+            name = f"{form.cleaned_data['first_name']} {form.cleaned_data['last_name']}"
+            context={"message": f"Account created. Welcome, {name.capitalize()}, to PG's Picsies!"}
+            return render(request, 'photostore/index.html', context=context)
         else:
-            return render(request, 'users/login.html', context={
-          "message" : "Invalid Credentials! Are you sure you did not miss anything ?"
-          })
+            return render(request, 'users/signup.html', context={"form": form})
 
     else:
-        return render(request, 'users/login.html')
-
-def signup(request):
-    return render(request, 'users/signup.html')
+        return render(request, 'users/signup.html', context={"form": RegForm()})
 
 
-def logout(request):
+
+# LOGIN
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    authentication_form = LoginForm
+    # to check def get_success_url
+    success_url = 'index'
+
+
+
+def user_profile(request):
+    return render(request, 'users/user_profile.html')
+
+
+
+# LOGOUT
+def logout_view(request):
     logout(request)
+    request.session.clear()
 
     return render(request, 'users/login.html',context={
           "message" : "Logged out successfully! Missed something ?"
