fd0684aca5ac7194c7bc3b8e1d615c78c32692f5
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-07 21:13:51 +0200

    user profile, customloginview

--- ecommstore/users/urls.py
+++ ecommstore/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from . import views
+from users.views import CustomLoginView
 
 # define namespace
 app_name = "users"
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', CustomLoginView.as_view(), name='login'),
+    path('my_profile', views.user_profile, name='user_profile'),
     path('signup', views.signup, name='signup'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.logout_view, name='logout'),
     path('redirect-to-home', views.redirect_to_home, name='redirect_to_home'),
 ]
