4988d7577cafbdd55d8a7acfb388c4e92a097532
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-09 19:42:18 +0200

    remove LoginForm, simplify login

--- ecommstore/users/forms.py
+++ ecommstore/users/forms.py
@@ -1,14 +1,13 @@
 from django import forms
-#from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import UserProfile
 
 
-class LoginForm(AuthenticationForm):
-    class Meta:
-        model = UserProfile
-        fields = ["username", "password"]
+# class LoginForm(AuthenticationForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ["username", "password"]
 
 
 class RegForm(forms.ModelForm):
