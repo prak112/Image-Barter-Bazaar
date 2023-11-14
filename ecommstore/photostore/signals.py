3f92c65113820ae316b1161499429a3c6224e426
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-14 17:55:28 +0200

    modify existing Customer instances

--- ecommstore/photostore/signals.py
+++ ecommstore/photostore/signals.py
@@ -1,14 +1,19 @@
-# to trigger signal function upon UserProfile instance modification
+# to trigger signal function upon UserProfile instance creation/modification
 from django.db.models.signals import post_save
-# to pass modified UserProfile instance
+# to pass created/modified UserProfile instance
 from django.dispatch import receiver 
 from users.models import UserProfile
 from photostore.models import Customer
 
 
+# UserProfile instance passed to create/modify Customer instance
 @receiver(post_save, sender=UserProfile)
-# modified UserProfile instance passed to modify Customer instance
 def create_customer_profile(sender, instance, created, **kwargs):
-    if created:
+    if created: # new Customer instance
         Customer.objects.create(user_info=instance)
-
+    
+    else:   # existing Customer instance
+        customer = Customer.objects.get(user_info=instance)
+        customer.full_name = f"{instance.first_name} {instance.last_name}".capitalize()
+        customer.customer_type = instance.user_type
+        customer.save()
