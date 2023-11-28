35b2490fda3e9cffdc9d6ca5c237faea4a68d5dd
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-28 22:29:14 +0200

    update SearchForm

--- ecommstore/photostore/forms.py
+++ ecommstore/photostore/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, 
                             required=True,
-                            widget=forms.TextInput(attrs={"placeholder": "Enter search like 'Mountains', 'Trees'..."}))
-    submit = forms.CharField(widget=forms.widgets.Input(attrs={'type':'submit', 'value':'Search'}))
+                            widget=forms.TextInput(attrs=
+                                                   {'placeholder': "Enter search like 'Mountains', 'Trees'...",
+                                                    'class': "search-bar"}
+                                                   ))
+    submit = forms.CharField(widget=forms.widgets.Input(attrs=
+                                                        {'type':'submit', 'value':'Search',
+                                                         'class':'search-button',
+                                                         }))
     
+
