b4fe5636d8c8bf1b4c23a5bf155dfa480e8ef72b
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-28 15:24:18 +0200

    unused now, for later

--- ecommstore/photostore/forms.py
+++ ecommstore/photostore/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, 
                             required=True,
-                            widget=forms.TextInput(attrs={"placeholder": "Enter search like 'Mountains near a river'"}))
+                            widget=forms.TextInput(attrs={"placeholder": "Enter search like 'Mountains', 'Trees'..."}))
     submit = forms.CharField(widget=forms.widgets.Input(attrs={'type':'submit', 'value':'Search'}))
     
