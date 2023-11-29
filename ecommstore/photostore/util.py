4abafd2b343b8c71b11848bd5934384e997ce6f2
PrakGov <55034062+prak112@users.noreply.github.com>
2023-11-29 17:29:51 +0200

    add theme_code, upd paginate exceptions

--- ecommstore/photostore/util.py
+++ ecommstore/photostore/util.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
+from urllib.parse import urlencode
 
+from photostore.models import Product
 
 def paginate(request, products_list, items_per_page):
     
@@ -9,7 +11,29 @@ def paginate(request, products_list, items_per_page):
         page_content = paginator.page(current_page)
     except PageNotAnInteger:
         page_content = paginator.page(1)
-    except EmptyPage:
+    except EmptyPage or InvalidPage:
         page_content = paginator.page(paginator.num_pages)
+    except ValueError:
+        # Modify URL to include filter conditions for filter_product
+        filter_params = {
+            'category': request.GET.get('category'),
+            'theme': get_theme_code(request.GET.get('theme')),
+            'author': request.GET.get('author'),
+        }
+        pagination_url = f"{request.path}?{urlencode(filter_params)}&page="
+        page_content.paginator.base_url = pagination_url
 
     return page_content
+
+
+
+
+def get_theme_code(theme_selected):
+    theme_choices = Product.THEME_CHOICES
+    selected_theme_code = ''
+    for code, theme in theme_choices:
+        if theme_selected == theme:
+            selected_theme_code = code
+    
+    return selected_theme_code
+    
