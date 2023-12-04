0f8cd6c4db7a5d2c9b29f7c956cf9ceb20c3fc99
PrakGov <55034062+prak112@users.noreply.github.com>
2023-12-04 22:52:07 +0200

    debug add-to-cart notification

--- ecommstore/photostore/static/photostore/scripts_checkout.js
+++ ecommstore/photostore/static/photostore/scripts_checkout.js
@@ -1,29 +1,25 @@
 // detect events on add-to-cart button
 document.querySelector('#add-to-cart').addEventListener('click', function() {
-    //notification banner
-    displayNotificationBanner();
-    window.location.href = '/photostore/products';
-    
-    // AJAX request to add_to_cart
-    // var request = new XMLHttpRequest();
-    // request.open('GET', '/photostore/add_to_cart/', true);
-    // request.onreadystatechange = function() {
-    //     if (request.readyState === XMLHttpRequest.DONE && request.status === 200){
-    //         // update DOM based on view 'add_to_cart' response
-    //         var response = JSON.parse(request.responseText);
-    //         if (response.success){
-    //             // notification banner
-    //             displayNotificationBanner();
-    //             window.location.href = '/photostore/products';
-    //         }
-    //     }
-    // };
-    // request.send();
+    // get product id
+    var product_id = this.getAttribute('product_id');
+
+    // send AJAX request to add_to_cart
+    fetch('/photostore/add-to-cart/product_id=' + product_id +' /')
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            displayNotificationBanner();
+            window.location.href = '/photostore/products/';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 });
 
 function displayNotificationBanner() {
     document.querySelector('.notification').style.display = 'block';
     setTimeout(function() {
-        document.querySelector('.notification').style.display = 'none';
+        document.querySelector('.notification').style.opacity = '0';
     }, 2000);
 }
