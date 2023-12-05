e9e7b4332c1c83faec85a10f4b62f15efa077c68
PrakGov <55034062+prak112@users.noreply.github.com>
2023-12-05 18:30:15 +0200

    route remove-from-cart

--- ecommstore/photostore/urls.py
+++ ecommstore/photostore/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('products', views.products, name='products'),
     path('filter_products', views.filter_products, name='filter_products'),
-    path('add-to-cart/<int:product_id>', views.add_to_cart, name='add-to-cart'),    
+    path('add-to-cart/<int:product_id>', views.add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<int:product_id>', views.remove_from_cart, name='remove-from-cart'),    
     path('checkout', views.checkout, name='checkout'),
-    path('payment/<str:choice>', views.payment, name='payment'),
+    path('payment', views.payment, name='payment'),
     path('about_us', views.about_us, name='about_us'),
     path('license', views.license, name='license'),
 ]
