1d731335f230cae7aca8431e83025f0f18e6e5d4
PrakGov <55034062+prak112@users.noreply.github.com>
2023-12-07 18:57:34 +0200

    register Cart

--- ecommstore/photostore/admin.py
+++ ecommstore/photostore/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from photostore.models import Customer, Product, Order#, OrderDetail
+from photostore.models import Customer, Product, Order, Cart
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Cart)
 admin.site.register(Order)
 #admin.site.register(OrderDetail)
