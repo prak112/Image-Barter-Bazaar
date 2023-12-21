70659a58fa26d2f57d66d794eec51f043072e83e
PrakGov <55034062+prak112@users.noreply.github.com>
2023-12-21 15:13:49 +0200

    debug redirection-Order,Cart

--- ecommstore/photostore/admin.py
+++ ecommstore/photostore/admin.py
@@ -12,7 +12,7 @@ from django.utils.html import format_html
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ("full_name", "customer_type")
-
+    list_filter = ("customer_type", )
 
 
 @admin.register(Product)
@@ -28,10 +28,19 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ("customer_info", "item", "quantity", )
-    list_display_links = ("item", )
+    list_display = ("view_customer_link", "item", "quantity", )
+    # list_display_links = None
     list_filter = ("item__title", )
 
+    def view_customer_link(self, obj):
+        url = (
+            reverse('admin:photostore_customer_changelist')
+            + "?"
+            + urlencode({"customer_type": f"{obj.customer_info.customer_type}"})
+        )
+        url_text = obj.customer_info.full_name
+        return format_html('<a href={}>{}</a>', url, url_text)
+    view_customer_link.short_description = "Customer"
 
 
 @admin.register(Order)
@@ -39,14 +48,12 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ("__str__", "view_cart_link", "order_date", "order_status", "payment")
     list_display_links = ("order_date", )
     
-    # view_on_site = True
-    # list_select_related = True
 
     def view_cart_link(self, obj):
         url = (
             reverse('admin:photostore_cart_changelist')
             + "?"
-            + urlencode({"order__customer_order__id":f"{obj.customer_order.id}"})
+            + urlencode({"item__title":f"{obj.customer_order.item.title}"})
         )        
         return format_html('<a href={}>Show</a>', url)
     view_cart_link.short_description = "Details"
