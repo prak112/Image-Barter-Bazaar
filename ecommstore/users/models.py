dc8ac40d11d46c49354899dff91bc28f34351362
PrakGov <55034062+prak112@users.noreply.github.com>
2024-01-03 15:38:43 +0200

    validate fields in signup form

--- ecommstore/users/models.py
+++ ecommstore/users/models.py
@@ -10,6 +10,9 @@ class UserProfile(AbstractUser):
 
     # AbstractUser model fields - first_name, last_name, email, username, password, is_staff, is_active, date_joined
     # define additional or custom fields
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
     user_type = models.CharField('Profession/Hobby', max_length=20, choices=UserType.choices)
     bio = models.TextField('Bio', max_length=200, null=True, blank=True)
 
