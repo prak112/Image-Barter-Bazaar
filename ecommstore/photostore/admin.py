59f562a16df40324d859c30ce1363dde5d9cb474
PrakGov <55034062+prak112@users.noreply.github.com>
2024-01-15 21:39:15 +0200

    add register, upd index,model registration

--- ecommstore/photostore/admin.py
+++ ecommstore/photostore/admin.py
@@ -10,34 +10,45 @@ from django.utils.html import format_html
 
 # admin site customisation
 class MyAdminSite(admin.AdminSite):
-    site_header = "PG Picsies Admin"
-    # index_title = "Welcome Admin!"
-    index_template = "admin/index.html"
-
-    def index(self, request):
-        models = [Customer, Product, Cart, Order]
-        extra_context = {"models": models}
-        return super().index(request, extra_context=extra_context)
-    
-
-admin_site = MyAdminSite(name="admin")
-admin_site.register(Customer)
-admin_site.register(Product)
-admin_site.register(Cart)
-admin_site.register(Order)
+    site_header = "PG Picsies Administration"
+    site_title = "Administration"
+    index_title = "PG Picsies"
+    app_index_template = "admin/index.html"
 
 
+    def register(self, model_or_iterable, admin_class=None, **options):
+        result = super().register(model_or_iterable, admin_class, **options)
+        return result
+
+
+    def index(self, request, extra_context=None):
+        app_dict = {}
+        for model, model_admin in self._registry.items():
+            app_label = model._meta.app_label
+            if app_label not in app_dict:
+                app_dict[app_label] = {}
+            app_dict[app_label][model.__name__] = model_admin
+
+        extra_context = extra_context or {}
+        extra_context['app_dict'] = app_dict
+        return super().index(request, extra_context)
+
+
+admin_site = MyAdminSite(name='customadmin')
 
 
 
-# Register your models here.
-@admin.register(Customer)
+# Model Registration, ModelAdmin customisation
+#@admin.site.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ("full_name", "customer_type")
     list_filter = ("customer_type", )
 
+admin_site.register(Customer, CustomerAdmin)
 
-@admin.register(Product)
+
+
+#@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "theme", "category", "view_image_link", )
     list_display_links = ("title", )
@@ -60,9 +71,11 @@ class ProductAdmin(admin.ModelAdmin):
 
     view_image_link.short_description = 'Image Preview'
 
+admin_site.register(Product, ProductAdmin)
 
 
-@admin.register(Cart)
+
+#@admin_site.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ("view_customer_link", "view_item_link", "quantity", )
     list_filter = ("item__title", )
@@ -89,9 +102,11 @@ class CartAdmin(admin.ModelAdmin):
         return format_html('<a href={} target="_blank">{}</a>'.format(url, url_text))
     view_item_link.short_description = "Item Preview"
 
+admin_site.register(Cart, CartAdmin)
 
 
-@admin.register(Order)
+
+#@admin_site.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("view_order_link", "view_cart_link", "order_date", "order_status", "payment")
     list_display_links = ("order_date", )
@@ -111,4 +126,8 @@ class OrderAdmin(admin.ModelAdmin):
         )        
         return format_html('<a href={}>Show</a>', url)
     view_cart_link.short_description = "View Cart"
-    
+
+admin_site.register(Order, OrderAdmin)
+
+
+
