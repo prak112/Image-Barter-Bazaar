a3b966e8e2b18a37a26bb9d0427ba0bbab84767a
PrakGov <55034062+prak112@users.noreply.github.com>
2024-10-01 22:23:18 +0300

    debug: read env variable

--- ecommstore/ecommstore/settings.py
+++ ecommstore/ecommstore/settings.py
@@ -26,7 +26,7 @@ env = environ.Env()
 env.read_env(os.path.join(BASE_DIR,'.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+DJANGO_SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Initialize environment variables
